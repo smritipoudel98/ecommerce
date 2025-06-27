d312b4e6ee0109616e052b55fd344148169d9502
Smriti Poudel <smritipoudel0607@gmail.com>
2025-06-27 14:01:48 +0545

    forget and reset the password

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,8 +47,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect()->route('login')->with('status', 'Registration successful. Please log in.');
+
     }
 }
